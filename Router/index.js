e1c342b3fa0cfb0909f60631d5037364cd381f36
Gunachandran <141514627+Guna2824@users.noreply.github.com>
2024-03-29 13:11:15 +0530

    Update index.js

--- Router/index.js
+++ Router/index.js
@@ -70,7 +70,7 @@ router.post("/orderfood", async (req, res) => {
 
   const foodAmount = unitPrice * count;
   const zoneAmount = (baseKmPrice + kmPrice) * typePrice;
-  const total = foodAmount + zoneAmount;
+  const total = Math.round(foodAmount + zoneAmount);
 
   const response = await [
     {
