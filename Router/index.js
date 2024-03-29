56897502296c8b781994adfc07176b44285ee230
Gunachandran <141514627+Guna2824@users.noreply.github.com>
2024-03-30 00:25:46 +0530

    Update index.js

--- Router/index.js
+++ Router/index.js
@@ -25,7 +25,7 @@ router.put("/additem", async (req, res) => {
         unit: unit,
         unitPrice: unitPrice,
       });
-      res.status(201).send("FoodItem Add DB " + response);
+      res.status(201).send("FoodItem Add DB ");
     } catch (err) {
       res.status(400).send(err.message);
       console.error(err);
